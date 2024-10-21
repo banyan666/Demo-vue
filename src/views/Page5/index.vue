e0947c64ef5dc83f31a8800aa7b9088060e48783
ban_yan <15029296293@163.com>
2024-10-21 14:18:29 +0800

    新增炫酷border

--- src/views/Page5/index.vue
+++ src/views/Page5/index.vue
@@ -16,6 +16,9 @@
     <div style="height: 50%;display: flex;justify-content: center;align-items: center">
       <div>{{ $t('message.hello') }}</div>
     </div>
+    <div style="width:100%;height:100px">
+      <div class="card">炫酷的box</div>
+    </div>
   </div>
 </template>
 
@@ -34,5 +37,52 @@ const languageChange = (value) => {
 </script>
 
 <style scoped>
+.card {
+  margin: 0 auto;
+  padding: 2rem;
+  width: 100px;
+  height: 100%;
+  background: #1c1f2b;
+  text-align: center;
+  border-radius: 10px;
+  position: relative;
+  color: #ffffff;
 
+}
+
+
+.card::after ,.card::before{
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /*background: red;*/
+  background-image: conic-gradient(from var(--angle), #05f57d, #e80b0b, #45ec3c);
+  top: 50%;
+  left: 50%;
+  translate: -50% -50%;
+  z-index: -1;
+  animation: 3s spin linear infinite;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+@property --angle {
+  syntax: "<angle>";
+  initial-value: 0deg;
+  inherits: false;
+}
+@keyframes spin {
+  from {
+    --angle: 0deg;
+  }
+  to {
+    --angle: 360deg;
+  }
+}
+
+.card::before {
+  filter: blur(1.5rem);
+  opacity: 1;
+}
 </style>
